54b2dd8b83d4ba0a4826669792adf083e2d6f41f
Luthor Sparks Costello <luthorsparks@gmail.com>
2026-06-01 05:30:01 -0500

    feat: request Auth0 access token with API audience

--- src/App.tsx
+++ src/App.tsx
@@ -45,6 +45,8 @@ const App = () => {
             clientId={auth0Config.clientId}
             authorizationParams={{
               redirect_uri: auth0Config.callbackUrl,
+              audience: auth0Config.audience || undefined,
+              scope: 'openid profile email',
             }}
             cacheLocation="localstorage"
           >
